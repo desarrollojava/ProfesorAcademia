a14369f06c503b62df839e02324797086704696e
Lorena <Lorena@DESKTOP-D0FMRO7>
2018-09-28 19:37:34 -0500

    cambios enviados

---
+++ src/main/resources/com/configuracion/datos.sql
@@ -0,0 +1,10 @@
+insert into persona(nombres,apellidos,direccionDomicilio)
+values('Ivan','Segovia','Quito');
+
+insert into persona(nombres,apellidos,direccionDomicilio)
+values('Maria','Caiza','Ibarra');
+
+insert into persona(nombres,apellidos,direccionDomicilio)
+values('Juan','Orosco','Valle');
+
+
---
+++ src/main/resources/com/configuracion/esquema.sql
@@ -0,0 +1,7 @@
+drop table if exists persona;
+create table persona(
+idPersona identity primary key,
+nombres varchar(30) not null,
+apellidos varchar(30) not null,
+direccionDomicilio varchar(30) not null
+);
